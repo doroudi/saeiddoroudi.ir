3371265c41101d787909eb654a6d74eb602ea956
Saeid Doroudi <doroudi@outlook.com>
2025-05-30 10:46:37 +0330

    add country api

--- src/pages/api/country.ts
+++
@@ -1,6 +0,0 @@
-export const get = ({ request }) => {
-    const country = request.headers.get('CF-IPCountry') || 'Unknown';
-    return new Response(JSON.stringify({ country }), {
-      headers: { 'Content-Type': 'application/json' }
-    });
-  };
